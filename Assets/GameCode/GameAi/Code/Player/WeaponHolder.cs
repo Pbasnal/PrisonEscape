d5aa106a07813df7f60841f007c8d63a080f6b5a
Pankaj Basnal <pbasnal@microsoft.com>
2020-03-29 10:01:56 +0530

    added weapon holder script

---
+++ Assets/GameCode/GameAi/Code/Player/WeaponHolder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameCode.GameAi.Code.Player
+{
+    public class WeaponHolder : MonoBehaviour
+    {
+        private IDictionary<string, Transform> WeaponHoldingPositions;
+
+        private const string WeaponPositionTag = "";
+
+        private const string WeaponPosDown = "WeaponPosDown";
+        private const string WeaponPosUp = "WeaponPosUp";
+        private const string WeaponPosLeft = "WeaponPosLeft";
+        private const string WeaponPosRight = "WeaponPosRight";
+
+        public void Awake()
+        {
+            WeaponHoldingPositions = new Dictionary<string, Transform>();
+
+            foreach (Transform child in transform)
+            {
+
+            }
+
+        }
+    }
+}
