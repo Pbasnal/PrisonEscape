e5babc565744485230e87bcaec4c561377db0522
Pankaj Basnal <pbasnal@microsoft.com>
2020-04-25 14:07:31 +0530

    Calculating motion state in rigidbody movement

--- Assets/GameCode/Mechanics/PlayerMechanics/RigidBodyMovement.cs
+++ Assets/GameCode/Mechanics/PlayerMechanics/RigidBodyMovement.cs
@@ -1,10 +1,12 @@
 ﻿using GameCode.Interfaces;
 using Pathfinding;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace GameCode.Mechanics.PlayerMechanics
-{ 
+{
+    [RequireComponent(typeof(Animator))]
     [RequireComponent(typeof(Rigidbody2D))]
     [RequireComponent(typeof(Seeker))]
     public class RigidBodyMovement : MonoBehaviour, ICanMove
@@ -21,12 +23,15 @@ namespace GameCode.Mechanics.PlayerMechanics
 
         private Seeker _seeker;
         private Rigidbody2D _rigidBody;
-
+        private Animator animator;
+        private int motionStateHash;
 
         private void Awake()
         {
             _seeker = GetComponent<Seeker>();
             _rigidBody = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+            motionStateHash = Animator.StringToHash("MotionState");
 
             _path = new List<Vector3>();
         }
@@ -84,7 +89,75 @@ namespace GameCode.Mechanics.PlayerMechanics
         public void Move(Vector2 target)
         {
             var dir = (target - _position).normalized;
+
+            Vector2 direction;
+
+            if (Mathf.Abs(dir.x) >= Mathf.Abs(dir.y))
+            {
+                direction = dir.x < 0 ? 
+                    SetAnimatorMotionStateLeft() 
+                    : SetAnimatorMotionStateRight();
+            }
+            else
+            {
+                direction = dir.y < 0 ?
+                    SetAnimatorMotionStateDown()
+                    : SetAnimatorMotionStateUp();
+            }
+
+            
+
             _rigidBody.velocity = dir * _moveSpeed * Time.deltaTime;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (_path.Count == 0)
+            {
+                return;
+            }
+
+            var dir = ((Vector2)_path[0] - _position).normalized;
+            var hit = Physics2D.Raycast(_position, dir, 1f);
+
+            if (hit.collider != null)
+            {
+                Debug.DrawLine(_position, hit.point, Color.red, 2);
+
+                //Debug.Log(string.Format("Direction: {0}, {1}  - object: {2}",
+                //dir.x, dir.y,
+                //hit.collider.name));
+                //Debug.Log("Can't move forward. Cleaning path");
+                _path.Clear();
+            }
+        }
+
+        private Vector2 SetAnimatorMotionStateDown()
+        {
+            animator.SetInteger(motionStateHash, 2);
+
+            return Vector2.down;
+        }
+
+        private Vector2 SetAnimatorMotionStateUp()
+        {
+            animator.SetInteger(motionStateHash, 1);
+
+            return Vector2.up;
+        }
+
+        private Vector2 SetAnimatorMotionStateLeft()
+        {
+            animator.SetInteger(motionStateHash, 3);
+
+            return Vector2.left;
+        }
+
+        private Vector2 SetAnimatorMotionStateRight()
+        {
+            animator.SetInteger(motionStateHash, 4);
+
+            return Vector2.right;
         }
 
         public void SetPathTo(Vector2 target)
@@ -107,28 +180,5 @@ namespace GameCode.Mechanics.PlayerMechanics
             _path = p.vectorPath;
             _path.RemoveAt(0);
         }
-
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (_path.Count == 0)
-            {
-                return;
-            }
-
-            var dir = ((Vector2)_path[0] - _position).normalized;
-            var hit = Physics2D.Raycast(_position, dir, 1f);
-
-            if (hit.collider != null)
-            {
-                Debug.DrawLine(_position, hit.point, Color.red, 2);
-
-                //Debug.Log(string.Format("Direction: {0}, {1}  - object: {2}",
-                    //dir.x, dir.y,
-                    //hit.collider.name));
-                //Debug.Log("Can't move forward. Cleaning path");
-                _path.Clear();
-            }
-        }
-
     }
 }
